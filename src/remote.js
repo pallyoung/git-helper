a3a1ad3e664328eee82e64d9e19e99b12a07eefb
Spencer <pallyoung@163.com>
2018-03-15 19:40:30 +0800

    rename

--- src/remote.js
+++ src/remote.js
@@ -12,12 +12,24 @@ function remoteVerbose() {
     var result = sh.execSync('git remote -v');
     if (!result.error) {
         var stdout = result.stdout.split('\n');
-        var fetch = stdout[0];
-        var push = stdout[1];
-        return {
-            fetch,
-            push
+        var verbose = {
+
         }
+        stdout.forEach(item => {
+            try {
+                var name = item.match(/([^\t]*?)\t/)[1];
+                var url = item.match(/\t([^\s]*?)\s/)[1];
+                var type = item.match(/\(([^\)]*?)\)/)[1];
+                if (!verbose[name]) {
+                    verbose[name] = {}
+                }
+                verbose[name][type] = url;
+            } catch (e) {
+                //ignore
+            }
+
+        });
+        return verbose;
     } else {
         throw new Error();
     }
@@ -25,8 +37,15 @@ function remoteVerbose() {
 function remoteAdd() {
 
 }
-function remoteRename() {
-
+function remoteRename(oldname, newname) {
+    var result = sh.execSync('git remote rename ' + oldname + ' ' + newname);
+    if(result.error){
+        throw new Error();
+    }else if(result.stderr){
+        throw new Error();
+    }else{
+        return true;
+    }
 }
 function remoteRemove() {
 
@@ -43,7 +62,7 @@ function remoteSetUrl() {
 function remoteGetUrl() {
 
 }
-console.log(remoteVerbose())
+console.log(remoteRename('b','origin'))
 module.exports = {
     remote,
     remoteVerbose
